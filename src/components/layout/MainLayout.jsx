8b4bd9513a2318d658033b12b78ff965c812fbd7
Gereltbaatar <mglgerelt@gmail.com>
2024-10-18 11:41:03 +0800

    zasvsr h w

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -2,7 +2,7 @@ import { HeaderMain, Header } from "../parts";
 
 const MainLayout = ({ children }) => {
   return (
-    <main className="h-full">
+    <main className="w-full md:h-full h-[950px] overflow-clip bg-blue">
       <HeaderMain />
       <Header />
       {children}
